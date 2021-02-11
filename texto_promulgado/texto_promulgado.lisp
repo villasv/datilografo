7f36ee58cb9b0da0396b3a9b55c63ac74721a881
Victor Villas <mail@victor.villas>
2021-02-11 15:05:22 -0300

    Remove linhas em branco ao fim do documento

--- texto_promulgado/texto_promulgado.lisp
+++ texto_promulgado/texto_promulgado.lisp
@@ -3,18 +3,21 @@
 (ql:quickload '(:lquery :plump :cl-ppcre))
 
 (defvar página (uiop:read-file-string "página.html"))
-(defvar documento (lquery:$ (initialize página) "#conteudoConst p"))
+(defvar elementos (lquery:$ (initialize página) "#conteudoConst p"))
 
 ;; Remove espaçamento excessivo
-(loop for p across documento do
+(loop for p across elementos do
   (plump:strip p))
-(loop for p across documento do
+(loop for p across elementos do
   (plump:traverse p
     (lambda (nó) (setf (plump:text nó)
       (cl-ppcre:regex-replace-all "\\s+" (plump:text nó) " ")))
     :test #'plump:text-node-p))
 
-(defvar conteúdo (format nil "~{~A~^~%~}" (coerce (lquery:$ documento (text)) 'list)))
+(defvar linhas (coerce (lquery:$ elementos (text)) 'list))
+(defvar conteúdo (format nil "~{~A~%~^~}"
+  (remove-if (lambda (s) (string= "" s)) linhas)))
+
 (with-open-file (saída "texto_promulgado.txt"
     :direction :output
     :if-exists :supersede
