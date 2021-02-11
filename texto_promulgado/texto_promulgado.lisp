bb9324358985f3f4f305efad3e85ee4a1a3ff5e7
Victor Villas <mail@victor.villas>
2021-02-11 12:24:35 -0300

    Remove espaçamento excessivo do texto promulgado

--- texto_promulgado/texto_promulgado.lisp
+++ texto_promulgado/texto_promulgado.lisp
@@ -1,10 +1,18 @@
 #!/usr/bin/env -S sbcl --script
 (load "~/.sbclrc")
-(ql:quickload :lquery)
+(ql:quickload '(:lquery :plump :cl-ppcre))
 
 (defvar página (uiop:read-file-string "página.html"))
-(defvar documento (lquery:$ (initialize página) "#conteudoConst"))
+(defvar documento (lquery:$ (initialize página) "#conteudoConst p"))
 
+;; Remove espaçamento excessivo
+(loop for p across documento do
+  (plump:strip p))
+(loop for p across documento do
+  (plump:traverse p
+    (lambda (nó) (setf (plump:text nó)
+      (cl-ppcre:regex-replace-all "\\s+" (plump:text nó) " ")))
+    :test #'plump:text-node-p))
 
 (defvar conteúdo (format nil "~{~A~^~%~}" (coerce (lquery:$ documento (text)) 'list)))
 (with-open-file (saída "texto_promulgado.txt"
