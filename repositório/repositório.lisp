fd6f00b6738dbe3b7dca92c5a295dd6422e2eba0
Victor Villas <mail@victor.villas>
2021-02-12 12:10:26 -0300

    Adiciona rotina de criação de repositório

---
+++ repositório/repositório.lisp
@@ -0,0 +1,23 @@
+#!/usr/bin/env -S sbcl --script
+(load "~/.sbclrc")
+(ql:quickload :legit)
+
+;; Limpeza do ambiente
+(ensure-directories-exist "./cartamagna/")
+(defvar diretório (first (directory "./cartamagna/")))
+(uiop:delete-directory-tree diretório :validate t)
+
+;; Criação do repositório
+(ensure-directories-exist diretório)
+(legit:git-init :directory diretório)
+(defvar repositório (make-instance 'legit:repository :location diretório))
+
+;; Configuração de submódulo
+
+;; Inicialização com o texto promulgado
+(uiop:copy-file
+  "../texto_promulgado/texto_promulgado.md"
+  "./cartamagna/CONSTITUIÇÃO.md")
+(legit:add repositório ".")
+(legit:commit repositório
+  "Constituição da República Federativa do Brasil")
